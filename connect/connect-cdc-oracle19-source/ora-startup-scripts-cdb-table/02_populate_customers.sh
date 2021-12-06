78bb0f028d8060baa0d9794d248a7213cb0c43ad
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-12-06 14:10:10 +0000

    updates
    Former-commit-id: 6806a43fdb39bb768a560bcfedc258897603ff6c [formerly 06ffed3fc96364258d1400946e0a7d91c9e4bdcd]
    Former-commit-id: 19c0dc8bd9e07b55d9acff2ab644734aeba1c4ed
    Former-commit-id: ae71d3f23819f59686e119158c847a731070a72b

--- connect/connect-cdc-oracle19-source/ora-startup-scripts-cdb-table/02_populate_customers.sh
+++ connect/connect-cdc-oracle19-source/ora-startup-scripts-cdb-table/02_populate_customers.sh
@@ -4,11 +4,11 @@ echo 'Populating CUSTOMERS table'
 
 sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLCDB  <<- EOF
 
-insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (1, 'Rica', 'Blaisdell', 'rblaisdell0@rambler.ru', 'Female', ' ', 'Universal optimal hierarchy', TO_TIMESTAMP('1753-01-01 12:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
-insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (2, 'Ruthie', 'Brockherst', 'rbrockherst1@ow.ly', 'Female', ' ', 'Reverse-engineered tangible interface', TO_TIMESTAMP('1753-01-01 12:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
-insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (3, 'Mariejeanne', 'Cocci', 'mcocci2@techcrunch.com', 'Female', ' ', 'Multi-tiered bandwidth-monitored capability', TO_TIMESTAMP('1753-01-01 12:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
-insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (4, 'Hashim', 'Rumke', 'hrumke3@sohu.com', 'Male', ' ', 'Self-enabling 24/7 firmware', TO_TIMESTAMP('1753-01-01 12:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
-insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (5, 'Hansiain', 'Coda', 'hcoda4@senate.gov', 'Male', ' ', 'Centralized full-range approach', TO_TIMESTAMP('1753-01-01 12:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
+insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (1, 'Rica', 'Blaisdell', 'rblaisdell0@rambler.ru', 'Female', ' ', 'Universal optimal hierarchy', TO_TIMESTAMP('1753-01-01 00:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
+insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (2, 'Ruthie', 'Brockherst', 'rbrockherst1@ow.ly', 'Female', ' ', 'Reverse-engineered tangible interface', TO_TIMESTAMP('1753-01-01 00:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
+insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (3, 'Mariejeanne', 'Cocci', 'mcocci2@techcrunch.com', 'Female', ' ', 'Multi-tiered bandwidth-monitored capability', TO_TIMESTAMP('1753-01-01 00:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
+insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (4, 'Hashim', 'Rumke', 'hrumke3@sohu.com', 'Male', ' ', 'Self-enabling 24/7 firmware', TO_TIMESTAMP('1753-01-01 00:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
+insert into CUSTOMERS (id, first_name, last_name, email, gender, club_status, comments, my_timestamp) values (5, 'Hansiain', 'Coda', 'hcoda4@senate.gov', 'Male', ' ', 'Centralized full-range approach', TO_TIMESTAMP('1753-01-01 00:00:00.000000000', 'YYYY-MM-DD HH24:MI:SS.FF'));
 
   exit;
 EOF
