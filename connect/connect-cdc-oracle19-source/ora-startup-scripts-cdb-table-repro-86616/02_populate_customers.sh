a8017469a210cd000fe5c741ca9d03a916588791
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-01-06 16:49:42 +0100

    repro
    Former-commit-id: 6544052554388a14427ba636f25f94e186fe21e5 [formerly 11e20d929e92ec215825f97df030fd197255000a]
    Former-commit-id: a88c1998398c2907b277f6e8b349636060350c5e
    Former-commit-id: 4c46ae0c9709b14ecf1b0be5dbc25acc549d9f4c

---
+++ connect/connect-cdc-oracle19-source/ora-startup-scripts-cdb-table-repro-86616/02_populate_customers.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo 'Populating TIP table'
+
+sqlplus C\#\#MYUSER/mypassword@//localhost:1521/ORCLCDB  <<- EOF
+
+INSERT INTO TIP (TIP_ID, TIP_TITLE, TIP_ABSTRACT, TIP_DESCRIPTION, SITE_ID,LAST_UPD_DATE, LAST_UPD_BY, OBSOLETE_FLAG) Values (15, 'A row with abstract and description', 'An abstract', 'A description', 9,TO_DATE('12/09/2012 23:30:39', 'MM/DD/YYYY HH24:MI:SS'), 'NOBODY', 'N');
+
+INSERT INTO TIP (TIP_ID, TIP_TITLE, SITE_ID,LAST_UPD_DATE, LAST_UPD_BY, OBSOLETE_FLAG) Values (16, 'A row without abstract and description', 9, TO_DATE('12/09/2012 23:30:39', 'MM/DD/YYYY HH24:MI:SS'), 'NOBODY', 'N');
+
+  exit;
+EOF
